26a08b804168a982c64373fdf25518fb639afcc7
morozko69 <stanislav@moroz.it>
2020-10-22 20:19:36 +0300

    Initial PR

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Станислав Ким`,
+    description: `Бизнес-консультант в ИТ`,
+    author: `@stmoroz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -28,9 +28,9 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+                  margin: `auto`,
+                  padding: `1.5em`,
+                  backgroundColor: `#042248`
         }}
       >
         <main>{children}</main>
